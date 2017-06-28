7b708d341def6a9657b643ede0f4b23a7f6d8f3f
yangyefei <13774364001@163.com>
2017-06-28 15:07:44 +0800

    yyf

--- src/test/java/com/web/test/httpDemo.java
+++ src/test/java/com/web/test/httpDemo.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.remote.http.HttpClient;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.squareup.okhttp.Headers;
 import com.squareup.okhttp.MediaType;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
@@ -24,27 +25,44 @@ public class httpDemo {
 	public static void main(String[] args) throws IOException {
 
 		OkHttpClient client = new OkHttpClient();
+		/**
+		 * post
+		 */
+		// MediaType mediaType = MediaType.parse("application/json");
+		// RequestBody body = RequestBody.create(mediaType,
+		// "{\r\n\"userId\":\"41bbf7037464d6379a198ecb39a8066b\",\r\n\"userAnswer\":\"1B;2C;3C;4B;5A;6A;7E;8E;9A,9B,9E,9F;\",\r\n\"areaId\":100\r\n}");
+		// Request request = new Request.Builder()
+		// .url("http://172.16.11.133:8097/yrb/fund/chooseAnswer")
+		// .post(body)
+		// .addHeader("content-type", "application/json")
+		// .addHeader("cache-control", "no-cache")
+		// .addHeader("postman-token", "42e1bdb2-4644-cbac-d861-9344eaf16b01")
+		// .build();
 
-		MediaType mediaType = MediaType.parse("application/json");
-		RequestBody body = RequestBody.create(mediaType, "{\r\n\"userId\":\"41bbf7037464d6379a198ecb39a8066b\",\r\n\"userAnswer\":\"1B;2C;3C;4B;5A;6A;7E;8E;9A,9B,9E,9F;\",\r\n\"areaId\":100\r\n}");
-		Request request = new Request.Builder()
-		  .url("http://172.16.11.133:8097/yrb/fund/chooseAnswer")
-		  .post(body)
-		  .addHeader("content-type", "application/json")
-		  .addHeader("cache-control", "no-cache")
-		  .addHeader("postman-token", "42e1bdb2-4644-cbac-d861-9344eaf16b01")
-		  .build();
-
+		/**
+		 * get
+		 */
+		Request request = new Request.Builder().url("http://172.16.11.133:8097/yrb/sysTag/isExistFwTag/1").build();
 		Response response = client.newCall(request).execute();
-		String Str = response.body().string();
-//        String[] str2=Str.split(",");
-//        List<String> list=Arrays.asList(str2);
-  
-        System.out.println(Str);
-        String jsonStr = "{id:2,apple:3}";
-        JSONObject jsonObject = JSONObject.fromObject(Str);
-       String id = (String) jsonObject.get("code");
-	System.out.println(id);
+		Headers responseHeaders = response.headers();
+		for (int i = 0; i < responseHeaders.size(); i++) {
+			System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
 		}
-	
+		String Str = response.body().string();
+		// String[] str2=Str.split(",");
+		// List<String> list=Arrays.asList(str2);
+
+		JSONObject jsonObject = JSONObject.fromObject(Str);
+
+		// JSONArray jsonArray = JSONArray.fromObject(list);
+		// JSONArray jsonArray = JSONArray.fromObject(object);
+		JSONObject jsonObject2 = new JSONObject();
+		jsonObject2.put("name", "yyf");
+
+		System.out.println(jsonObject.toString());
+		System.out.println(jsonObject2.optString("name"));
+		// String id = (String) jsonObject.get("code");
+		// System.out.println(id);
+	}
+
 }
