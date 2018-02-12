70209753870e372cb3dd6ea4a8a29e9bc989b6d0
xmsu <xmsu@Ctrip.com>
2018-02-12 14:03:24 +0800

    C1309630星级分别为<=2星搜索

--- src/main/java/test/apptest/hotel/StarSearch.java
+++ src/main/java/test/apptest/hotel/StarSearch.java
@@ -69,6 +69,33 @@ public class StarSearch extends BaseTest {
 	    }
 	}
 	
+	@Test(description = "by sxm: C1309630星级分别为<=2星搜索", groups = { "Base" })
+	public void twostarSearch() throws Exception {
+		new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("myctrip_hotel_icon"))).click();
+		new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("tv_stay_in"))).click();
+	    new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("hotel_destination_search_keyword_import"))).clear();
+	    new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("hotel_destination_search_keyword_import")))
+	                      .sendKeys("上海"); 
+	     ArrayList<WebElement> destinationlist = (ArrayList<WebElement>) driver.findElements(By.id("tvTitle"));
+	     destinationlist.get(0).click(); 
+	     
+		 //清除酒店首页的成人儿童筛选
+	     hotelHomePageInitialImpl.HotelPageAdultsChildFilter(driver);
+	     
+	     new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("rl_star_price"))).click();
+	     new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(By.id("view_hotel_filter_rating_no_limit"))).click();
+	     new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(By.id("view_hotel_filter_rating_2"))).click();
+	     new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("tv_done"))).click();
+	     new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(By.id("tv_search"))).click(); 
+	     try {
+	    	 WebElement filterreddot= driver.findElement(By.id("top_bar_filter_red_dot"));
+	    	 Assert.assertTrue(filterreddot.isDisplayed());
+	    	 logger.info("C1309630 星级分别为<=2星搜索成功");
+	     } catch (Exception e) 
+	    {
+	    	logger.info("C1309630星级分别为<=2星搜索失败");
+	    }
+	}
 	@AfterClass
 	public void afterClass() {
 		logger.info("I here afterclass");
