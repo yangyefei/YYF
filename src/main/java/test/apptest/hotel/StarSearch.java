357d610db2676cd3e9331f97ce578567764f0124
yulf <yulf@Ctrip.com>
2018-02-13 17:22:53 +0800

    新增 defaultStarSearch

--- src/main/java/test/apptest/hotel/StarSearch.java
+++ src/main/java/test/apptest/hotel/StarSearch.java
@@ -4,6 +4,8 @@ import static org.testng.AssertJUnit.assertEquals;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import io.appium.java_client.android.AndroidDriver;
@@ -25,6 +27,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.log4j.Logger;
+import org.aspectj.lang.annotation.After;
+
 import java.io.File;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -96,6 +100,76 @@ public class StarSearch extends BaseTest {
 	    	logger.info("C1309630星级分别为<=2星搜索失败");
 	    }
 	}
+	
+	@Test(description = "by ylf: C1309627	默认价格搜索，默认星级", groups = { "Base" })
+	public void defaultStarSearch() throws Exception {
+		logger.info("---搜 上海---");
+		new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("myctrip_hotel_icon"))).click();
+		new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("tv_stay_in"))).click();
+	    new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("hotel_destination_search_keyword_import"))).clear();
+	    new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("hotel_destination_search_keyword_import")))
+	                      .sendKeys("上海"); 
+	     ArrayList<WebElement> destinationlist = (ArrayList<WebElement>) driver.findElements(By.id("tvTitle"));
+	     destinationlist.get(0).click(); 
+	     
+		 //清除酒店首页的成人儿童筛选
+	     logger.info("---清除酒店首页的成人儿童筛选---");
+	     hotelHomePageInitialImpl.HotelPageAdultsChildFilter(driver);
+	     
+	     //搜2星到列表页
+	     logger.info("---搜3星到列表页---");
+	     new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("rl_star_price"))).click();
+	     new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(By.id("view_hotel_filter_rating_no_limit"))).click();
+	     new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(By.id("view_hotel_filter_rating_3"))).click();
+	     new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("tv_done"))).click();
+	     new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(By.id("tv_search"))).click(); 
+	     
+	     //返回搜索首页
+	     logger.info("---返回搜索首页---");
+	     new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("iv_back"))).click();
+	     //返回Trip首页	     
+	     logger.info("---返回Trip首页---");
+	     driver.findElementByClassName("android.widget.ImageButton").click();
+	     //进入酒店搜索首页
+	     logger.info("---进入酒店搜索首页---");
+	     new WebDriverWait(driver, 120).until(ExpectedConditions.elementToBeClickable(By.id("myctrip_hotel_icon"))).click();
+	     
+	     //2星仍在
+	     try {
+	    	 logger.info("---判断3星仍在---");
+	    	 WebElement star3= driver.findElement(By.id("tv_star_text"));
+	    	 Assert.assertTrue(star3.getText().equals("3"));
+	    	 logger.info("默认3星成功");
+	     } catch (Exception e) 
+	    {
+	    	logger.info("默认3星失败");
+	    }	     
+	     //进入2星列表页
+	     try {
+	    	 new WebDriverWait(driver, 60).until(ExpectedConditions.elementToBeClickable(By.id("tv_search"))).click(); 
+	    	 logger.info("---进入3星列表页，判断筛选红点---");
+	    	 WebElement filterreddot= driver.findElement(By.id("top_bar_filter_red_dot"));
+	    	 Assert.assertTrue(filterreddot.isDisplayed());
+	    	 logger.info("C1309627	默认价格搜索，默认星级成功");
+
+		     
+	     } catch (Exception e) 
+	    {
+	    	logger.info("C1309627	默认价格搜索，默认星级失败");
+	    }
+
+
+	}
+	
+	@AfterMethod
+	public void afterTest() {
+	     //返回搜索首页
+	     logger.info("---返回搜索首页---");
+	     new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("iv_back"))).click();
+	     //返回Trip首页	     
+	     logger.info("---返回Trip首页---");
+	     driver.findElementByClassName("android.widget.ImageButton").click();
+	}	
 	@AfterClass
 	public void afterClass() {
 		logger.info("I here afterclass");
