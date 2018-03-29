bc2543813a1770b45ebf68f67073295c5c4fc54b
Fabin Yan <fbyan@ctrip.com>
2018-03-29 14:58:09 +0800

    去掉显式的指定"udid"，以便各个手机都可以跑测试

--- src/main/java/service/impl/InitialServiceImpl.java
+++ src/main/java/service/impl/InitialServiceImpl.java
@@ -41,7 +41,7 @@ public class InitialServiceImpl implements InitialService {
 		// capabilities.setCapability("deviceName", "emulator-5554");
 		// capabilities.setCapability("deviceName","device");
 		// capabilities.setCapability("automationName","Appium");
-		capabilities.setCapability("udid", "127.0.0.1:62025");
+//		capabilities.setCapability("udid", "127.0.0.1:62025");
 		capabilities.setCapability("newCommandTimeout", "10");
 		capabilities.setCapability("unicodeKeyboard", "True");
 		capabilities.setCapability("resetKeyboard", "True");
