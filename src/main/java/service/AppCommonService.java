db611b8f2e0bdfad635b10b319b3339d2a1aeb72
Fabin Yan <fbyan@ctrip.com>
2018-04-08 16:21:13 +0800

    格式化代码

--- src/main/java/service/AppCommonService.java
+++ src/main/java/service/AppCommonService.java
@@ -4,65 +4,68 @@ package service;
 import io.appium.java_client.AppiumDriver;
 
 
-
 public interface AppCommonService {
-	
-	/**
-	 * app登录
-	 * @param driver
-	 * @param userName
-	 * @param userPassWord
-	 * @return
-	 */
-	public AppiumDriver loginForApp(AppiumDriver driver, String userName, String userPassWord); 
-	
-	/**
-	 * 退出app,无需确认是否已登录
-	 * @param driver
-	 * @return AppiumDriver
-	 */
-	public AppiumDriver logoutForApp(AppiumDriver driver);
-	
-	
-	/**
-	 * 评论并且提交
-	 * @param driver
-	 * @param comment
-	 * @return
-	 */
-	public AppiumDriver commentSubmit(AppiumDriver driver,String comment);
-	
-	/**
-	 * 向下滑动
-	 * @param driver
-	 * @return
-	 */
-	public AppiumDriver swipeToDown(AppiumDriver driver);
-	
 
-	/**
-	 * 滚动并查找要找的内容
-	 * @param driver
-	 * @param searchName
-	 * @param nameId
-	 * @param totalNumId
-	 * @return
-	 */
-	public AppiumDriver scrollAndFindName(AppiumDriver driver, String searchName, String nameId, String totalNum);
-	
-	/*
-	 * 酒店首页搜索
-	 * @param driver
-	 * @param keyword
-	 * */
-	public AppiumDriver homeSearchHotel(AppiumDriver driver, String keyword) throws InterruptedException;
+    /**
+     * app登录
+     *
+     * @param driver
+     * @param userName
+     * @param userPassWord
+     * @return
+     */
+    public AppiumDriver loginForApp(AppiumDriver driver, String userName, String userPassWord);
 
-	public AppiumDriver swipeToDown(AppiumDriver driver, int during, int num);
+    /**
+     * 退出app,无需确认是否已登录
+     *
+     * @param driver
+     * @return AppiumDriver
+     */
+    public AppiumDriver logoutForApp(AppiumDriver driver);
 
-	/*
-	 * 列表页到详情页
-	 * @param driver
-	 * */
-	public AppiumDriver listToDetail(AppiumDriver driver) throws InterruptedException;
-	
+
+    /**
+     * 评论并且提交
+     *
+     * @param driver
+     * @param comment
+     * @return
+     */
+    public AppiumDriver commentSubmit(AppiumDriver driver, String comment);
+
+    /**
+     * 向下滑动
+     *
+     * @param driver
+     * @return
+     */
+    public AppiumDriver swipeToDown(AppiumDriver driver);
+
+
+    /**
+     * 滚动并查找要找的内容
+     *
+     * @param driver
+     * @param searchName
+     * @param nameId
+     * @return
+     */
+    public AppiumDriver scrollAndFindName(AppiumDriver driver, String searchName, String nameId, String totalNum);
+
+    /*
+     * 酒店首页搜索
+     * @param driver
+     * @param keyword
+     * */
+    public AppiumDriver homeSearchHotel(AppiumDriver driver, String keyword) throws InterruptedException;
+
+    public AppiumDriver swipeToDown(AppiumDriver driver, int during, int num);
+
+    /*
+     * 列表页到详情页
+     * @param driver
+     * */
+    public AppiumDriver listToDetail(AppiumDriver driver) throws InterruptedException;
+
 }
