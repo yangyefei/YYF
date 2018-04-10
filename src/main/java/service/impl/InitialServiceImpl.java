a98e5c9a88058b5b46c7317132385ec4819f0c70
13774364001 <yefeiyang@Ctrip.com>
2018-04-10 20:07:57 +0800

    修改用例

--- src/main/java/service/impl/InitialServiceImpl.java
+++ src/main/java/service/impl/InitialServiceImpl.java
@@ -11,24 +11,23 @@ import service.InitialService;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
 public class InitialServiceImpl implements InitialService {
 
-    private String AppPackage = "ctrip.english";
+	private String AppPackage = "ctrip.english";
 
-    private String AppActivity = "com.ctrip.ibu.myctrip.main.module.home.IBUHomeActivity";
+	private String AppActivity = "com.ctrip.ibu.myctrip.main.module.home.IBUHomeActivity";
 
-    private String platformVersion;
+	private String platformVersion;
 
-    private String appRunMachineIp = "127.0.0.1";
+	private String appRunMachineIp = "127.0.0.1";
 
-    private String webRunMachineIp = "127.0.0.1";
+	private String webRunMachineIp = "127.0.0.1";
 
-    private String appiumPort = "4723";
+	private String appiumPort = "4723";
 
-
-    @Override
-    public AndroidDriver<WebElement> appiumAndroidCtripSetUp(AndroidDriver driver, String AppPackage) throws MalformedURLException {
+	@Override
+	public AndroidDriver<WebElement> appiumAndroidCtripSetUp(AndroidDriver driver, String AppPackage)
+			throws MalformedURLException {
 
 		DesiredCapabilities capabilities = new DesiredCapabilities();
 		capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
@@ -37,92 +36,91 @@ public class InitialServiceImpl implements InitialService {
 		// capabilities.setCapability(Mobi, "7.1");
 		// capabilities.setCapability("deviceName", "emulator-5554");
 		// capabilities.setCapability("deviceName","device");
-		// capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("automationName", "UiAutomator2");
+		// capabilities.setCapability("automationName", "Appium");
+		capabilities.setCapability("automationName", "UiAutomator2");
 //		capabilities.setCapability("udid", "127.0.0.1:62025");
 		capabilities.setCapability("newCommandTimeout", "10");
 		capabilities.setCapability("unicodeKeyboard", "True");
 		capabilities.setCapability("resetKeyboard", "True");
-		capabilities.setCapability("autoAcceptAlerts", "True");	
+		capabilities.setCapability("autoAcceptAlerts", "True");
 		capabilities.setCapability("noReset", true);
 		capabilities.setCapability("platformVersion", platformVersion);
 		// capabilities.setCapability("udid",
 		// "emulator-5554");//如果要远程调用模拟器，这个参数必须要有
 		// capabilities.setCapability("app", app.getAbsolutePath());
+		// capabilities.setCapability("app", apkPath);
+		capabilities.setCapability("appPackage", AppPackage);
+		capabilities.setCapability("appActivity", AppActivity);
+		capabilities.setCapability("noSign", "True");
 
-//		capabilities.setCapability("app", apkPath);
-        capabilities.setCapability("appPackage", AppPackage);
-        capabilities.setCapability("appActivity", AppActivity);
-        capabilities.setCapability("noSign", "True");
+		return new AndroidDriver<>(new URL("http://" + appRunMachineIp + ":" + appiumPort + "/wd/hub"), capabilities);
+	}
 
-        return new AndroidDriver<>(new URL("http://" + appRunMachineIp + ":" + appiumPort + "/wd/hub"), capabilities);
-    }
+	@Override
+	public WebDriver browserOfInternetSetUp(WebDriver driver) throws Exception {
+		// TODO Auto-generated method stub
 
-    @Override
-    public WebDriver browserOfInternetSetUp(WebDriver driver) throws Exception {
-        // TODO Auto-generated method stub
+		driver = new RemoteWebDriver(new URL("http://" + webRunMachineIp + ":4444/wd/hub"),
+				DesiredCapabilities.internetExplorer());
 
-        driver = new RemoteWebDriver(new URL("http://" + webRunMachineIp + ":4444/wd/hub"),
-                DesiredCapabilities.internetExplorer());
+		return driver;
+	}
 
-        return driver;
-    }
+	@Override
+	public WebDriver browserOfChromeSetUp(WebDriver driver) throws Exception {
+		// TODO Auto-generated method stub
+		driver = new RemoteWebDriver(new URL("http://" + webRunMachineIp + ":4444/wd/hub"),
+				DesiredCapabilities.chrome());
 
-    @Override
-    public WebDriver browserOfChromeSetUp(WebDriver driver) throws Exception {
-        // TODO Auto-generated method stub
-        driver = new RemoteWebDriver(new URL("http://" + webRunMachineIp + ":4444/wd/hub"),
-                DesiredCapabilities.chrome());
+		return driver;
+	}
 
-        return driver;
-    }
+	@Override
+	public WebDriver browserOfFirefoxSetUp(WebDriver driver) throws Exception {
+		// TODO Auto-generated method stub
+		driver = new RemoteWebDriver(new URL("http://" + webRunMachineIp + ":4444/wd/hub"),
+				DesiredCapabilities.firefox());
+		return driver;
+	}
 
-    @Override
-    public WebDriver browserOfFirefoxSetUp(WebDriver driver) throws Exception {
-        // TODO Auto-generated method stub
-        driver = new RemoteWebDriver(new URL("http://" + webRunMachineIp + ":4444/wd/hub"),
-                DesiredCapabilities.firefox());
-        return driver;
-    }
+	public String getPlatformVersion() {
+		return platformVersion;
+	}
 
-    public String getPlatformVersion() {
-        return platformVersion;
-    }
+	public void setPlatformVersion(String platformVersion) {
+		this.platformVersion = platformVersion;
+	}
 
-    public void setPlatformVersion(String platformVersion) {
-        this.platformVersion = platformVersion;
-    }
+	public void setAppActivity(String appActivity) {
+		AppActivity = appActivity;
+	}
 
-    public void setAppActivity(String appActivity) {
-        AppActivity = appActivity;
-    }
+	public void setAppPackage(String appPackage) {
+		AppPackage = appPackage;
+	}
 
-    public void setAppPackage(String appPackage) {
-        AppPackage = appPackage;
-    }
+	public String getAppiumPort() {
+		return appiumPort;
+	}
 
-    public String getAppiumPort() {
-        return appiumPort;
-    }
+	public void setAppiumPort(String appiumPort) {
+		this.appiumPort = appiumPort;
+	}
 
-    public void setAppiumPort(String appiumPort) {
-        this.appiumPort = appiumPort;
-    }
+	public String getAppRunMachineIp() {
+		return appRunMachineIp;
+	}
 
-    public String getAppRunMachineIp() {
-        return appRunMachineIp;
-    }
+	public void setAppRunMachineIp(String appRunMachineIp) {
+		this.appRunMachineIp = appRunMachineIp;
+	}
 
-    public void setAppRunMachineIp(String appRunMachineIp) {
-        this.appRunMachineIp = appRunMachineIp;
-    }
+	public String getWebRunMachineIp() {
+		return webRunMachineIp;
+	}
 
-    public String getWebRunMachineIp() {
-        return webRunMachineIp;
-    }
-
-    public void setWebRunMachineIp(String webRunMachineIp) {
-        this.webRunMachineIp = webRunMachineIp;
-    }
+	public void setWebRunMachineIp(String webRunMachineIp) {
+		this.webRunMachineIp = webRunMachineIp;
+	}
 
 }
