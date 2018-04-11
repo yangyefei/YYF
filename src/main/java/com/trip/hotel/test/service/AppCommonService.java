04decc7e18c70bef466b698146dbea45f935972b
Fabin Yan <fbyan@ctrip.com>
2018-04-11 17:40:36 +0800

    Move package: service -> com.trip.hotel.test.service

---
+++ src/main/java/com/trip/hotel/test/service/AppCommonService.java
@@ -0,0 +1,76 @@
+package com.trip.hotel.test.service;
+
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+
+
+public interface AppCommonService {
+
+    /**
+     * app登录
+     *
+     * @param driver
+     * @param userName
+     * @param userPassWord
+     * @return
+     */
+    public AppiumDriver loginForApp(AppiumDriver driver, String userName, String userPassWord);
+
+    /**
+     * 退出app,无需确认是否已登录
+     *
+     * @param driver
+     * @return AppiumDriver
+     */
+    public AppiumDriver logoutForApp(AppiumDriver driver);
+
+
+    /**
+     * 评论并且提交
+     *
+     * @param driver
+     * @param comment
+     * @return
+     */
+    public AppiumDriver commentSubmit(AppiumDriver driver, String comment);
+
+    /**
+     * 向下滑动
+     *
+     * @param driver
+     * @return
+     */
+    public AppiumDriver swipeToDown(AppiumDriver driver);
+
+
+    /**
+     * 滚动并查找要找的内容
+     *
+     * @param driver
+     * @param searchName
+     * @param nameId
+     * @return
+     */
+    public AppiumDriver scrollAndFindName(AppiumDriver driver, String searchName, String nameId, String totalNum);
+
+    /*
+     * 酒店首页搜索
+     * @param driver
+     * @param keyword
+     * */
+    public AppiumDriver homeSearchHotel(AppiumDriver driver, String keyword) throws InterruptedException;
+
+    public AppiumDriver swipeToDown(AppiumDriver driver, int during, int num);
+
+    /*
+     * 列表页到详情页
+     * @param driver
+     * */
+    public AppiumDriver listToDetail(AppiumDriver driver) throws InterruptedException;
+
+    void changeLanguageTo(AndroidDriver<WebElement> driver, String targetLanguage);
+
+    void gotoHotelBook(AndroidDriver<WebElement> driver, int cityId, int hotelId) throws InterruptedException;
+}
--- src/main/java/service/impl/HotelHomePageInitialImpl.java
+++ src/main/java/service/impl/HotelHomePageInitialImpl.java
@@ -1,18 +1,11 @@
 package service.impl;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
-import service.HotelHomePageInitial;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.stereotype.Component;
-import org.apache.log4j.Logger;
+import service.HotelHomePageInitial;
 
 public class HotelHomePageInitialImpl implements HotelHomePageInitial {
 	
