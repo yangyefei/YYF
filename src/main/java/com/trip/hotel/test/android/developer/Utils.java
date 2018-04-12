d35333628e420af94bf6c2c27c6d0f538fa51273
Fabin Yan <fbyan@ctrip.com>
2018-04-12 12:10:19 +0800

    去掉注解

--- src/main/java/com/trip/hotel/test/android/developer/Utils.java
+++ src/main/java/com/trip/hotel/test/android/developer/Utils.java
@@ -1,11 +1,9 @@
 package com.trip.hotel.test.android.developer;
 
-import com.sun.istack.internal.NotNull;
 import org.openqa.selenium.By;
 
 public class Utils {
-    @NotNull
-    public static String getIdString(@NotNull By.ById byId) {
+    public static String getIdString(By.ById byId) {
         return byId.toString().substring("By.id: ".length());
     }
 }
